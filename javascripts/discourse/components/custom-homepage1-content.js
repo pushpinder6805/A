c70cabafb2704e4f900ae58cc786390f9d695f07
pinder99 <pinder.chaudhary@gmail.com>
2024-10-09 19:24:41 +0530

    Update custom-homepage1-content.js

--- javascripts/discourse/components/custom-homepage1-content.js
+++ javascripts/discourse/components/custom-homepage1-content.js
@@ -6,7 +6,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['/c/test', '/c/test'];
+    const customPaths = ['/c/member-map/14', 'https://discourse-support.online/c/member-map/14'];
     
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return customPaths.includes(currentURL);
