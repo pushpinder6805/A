f7ac0e163719a7d5ac89fb05eacd8ca5af24b3f6
pinder99 <pinder.chaudhary@gmail.com>
2024-10-09 19:10:19 +0530

    map

--- javascripts/discourse/components/custom-homepage1-content.js
+++ javascripts/discourse/components/custom-homepage1-content.js
@@ -6,7 +6,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['/c/free-educational-videos/20/none', '/c/free-educational-videos/none'];
+    const customPaths = ['/c/member-map/14', 'https://discourse-support.online/c/member-map/14'];
     
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return customPaths.includes(currentURL);
