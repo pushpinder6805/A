659ac08f73ae699d06b6026a266528bba9ab9639
pinder99 <pinder.chaudhary@gmail.com>
2024-10-17 22:50:52 +0530

    Update custom-homepage8-content.js

--- javascripts/discourse/components/custom-homepage8-content.js
+++ javascripts/discourse/components/custom-homepage8-content.js
@@ -6,7 +6,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPaths = ['/c/dr-whiskers/11', '/c/dr-whiskers/11'];
+    const customPaths = ['/c/d', '/c/fd'];
     
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return customPaths.includes(currentURL);
